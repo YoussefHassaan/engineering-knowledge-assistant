5d300a0cd16a736689591f27e11bcfaa40729f1c
YoussefHassaan <youssefhassaan99@gmail.com>
2026-06-27 05:06:21 +0300

    week 1 complete: employee CRUD API with PostgreSQL

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI , HTTPException
 from pydantic import BaseModel
+import psycopg2
+import datetime
 
 app = FastAPI()
+
+conn = psycopg2.connect(
+    dbname="fastapi",
+    user="postgres",
+    password="2407",
+    host="localhost"
+)
+cur = conn.cursor()
 
 employees = []
 
@@ -16,35 +26,46 @@ class employee(createEmployee):
 
 @app.get("/employees")
 async def get_employees():
-    return  employees
+    cur.execute("SELECT * FROM employees")
+    employees = cur.fetchall()
+    employees = [dict(zip([column[0] for column in cur.description], row)) for row in employees]
+    return employees
 
 @app.get("/employees/{employee_id}")
 async def get_employee(employee_id: int):
-    for employee in employees:
-        if employee.id == employee_id:
-            return employee
+    cur.execute("SELECT * FROM employees WHERE id = %s", (employee_id,))
+    employee = cur.fetchone()
+    if employee:
+        employee = dict(zip([column[0] for column in cur.description], employee))
+        return employee
     raise HTTPException(status_code=404, detail="Employee not found")
 
 @app.post("/employees")
 async def create_employee(new_employee: createEmployee):
-    new_id = len(employees) + 1
-    new_employee = employee(id=new_id, **new_employee.model_dump())
-    employees.append(new_employee)
-    return new_employee
+    cur.execute("INSERT INTO employees (name, email, department, salary) VALUES (%s, %s, %s, %s) RETURNING *", (new_employee.name, new_employee.email, new_employee.department, new_employee.salary))
+    added_employee = cur.fetchone()
+    added_employee = dict(zip([column[0] for column in cur.description], added_employee))
+    conn.commit()
+    return added_employee
 
 @app.put("/employees/{employee_id}")
 async def update_employee(employee_id: int, updated_employee: createEmployee):
-    for index,emp in enumerate(employees):
-        if emp.id == employee_id:
-            employees[index] = employee(id=employee_id, **updated_employee.model_dump())
-            return employees[index]
-    raise HTTPException(status_code=404, detail="Employee not found")
+    cur.execute("UPDATE employees SET name =  %s, email = %s, department = %s, salary = %s WHERE id = %s RETURNING *", (updated_employee.name, updated_employee.email, updated_employee.department, updated_employee.salary, employee_id))
+    if cur.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    updated_employee_data = cur.fetchone()
+    updated_employee_data = dict(zip([column[0] for column in cur.description], updated_employee_data))
+    conn.commit()
+
+    return updated_employee_data
 
 @app.delete("/employees/{employee_id}")
 async def delete_employee(employee_id: int):
-    for index, emp in enumerate(employees):
-        if emp.id == employee_id:
-            del employees[index]
-            return {"detail": "Employee deleted"}
-    raise HTTPException(status_code=404, detail="Employee not found")
+    cur.execute("DELETE FROM employees WHERE id = %s RETURNING *", (employee_id,))
+    if cur.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    deleted_employee_data = cur.fetchone()
+    deleted_employee_data = dict(zip([column[0] for column in cur.description], deleted_employee_data))
+    conn.commit()
+    return deleted_employee_data
 
